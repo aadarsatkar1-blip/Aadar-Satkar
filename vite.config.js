cb8aa8f0b54c3534174084974bd65d12ff8ebc66
[company system] <[aadarsatkar1@gmail.com]>
2025-12-08 17:50:51 +0530

    my finel commit on 8 dec 2025 time- 6:00pm

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
+// Curly braces {} hata do:
+import viteMinify from 'vite-plugin-html-minifier-terser';
 
 export default defineConfig({
+  plugins: [
+    viteMinify(),
+  ],
   build: {
     rollupOptions: {
-      input: glob.sync('**/*.html'), // Ye saari HTML files dhoondh lega (pages folder wali bhi)
+      input: glob.sync('**/*.html'),
     },
   },
 });
+
+
+
+
+
